80d081977d1c9c1bb487f494bc4088edc801accc
Ramana Bandapu <bandapu.ramana@gmail.com>
2022-09-28 14:48:05 +0530

    Redux setup completed

---
+++ racial-lip-1108/src/Redux/AuthReducer/reducer.js
@@ -0,0 +1,42 @@
+import * as types from "./actionTypes";
+
+const initialState = {
+  isAuth: false,
+  token: "",
+  isAuthLoading: false,
+  isAuthError: false,
+};
+
+const reducer = (oldState = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case types.USER_LOGIN_REQUEST: {
+      return {
+        ...oldState,
+        isAuthLoading: true,
+      };
+    }
+
+    case types.USER_LOGIN_SUCCESS: {
+      return {
+        ...oldState,
+        isAuthLoading: false,
+        isAuth: true,
+        token: payload,
+      };
+    }
+
+    case types.USER_LOGIN_FAILURE: {
+      return {
+        ...oldState,
+        isAuthLoading: false,
+        isAuthError: true,
+        isAuth: false,
+        token: "",
+      };
+    }
+    default: {
+      return oldState;
+    }
+  }
+};
