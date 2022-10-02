a04ea882b91e53840c25f7b28e80623ef717225d
Aditya Raj <aadiyadav56837492@gmail.com>
2022-10-02 09:59:22 +0530

    latest

---
+++ racial-lip-1108/src/Aadicomp/Allcomp/Allcomp.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from "../Navbar/Navbar"
+import Topimg from "../Topimage/Topimg"
+import TrendingFundraisers from "../Trending/TrendingFundraisers"
+import Start from "../start/Start"
+import Buttoncomp from "../Button/Button"
+import Monthly from "../Monthlydonation/Monthly"
+import Success from "../Sucess/Success"
+import Footer from '../../Footer/Footer'
+
+const Allcomp = () => {
+  return (
+    <div>
+   <Navbar/>
+    <Topimg/>
+     <br/>
+     <TrendingFundraisers/>
+     <Start/>
+     <br/>
+     <br/>
+     <Buttoncomp/>
+     <Monthly/>
+     <Success/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default Allcomp
--- racial-lip-1108/src/App.js
+++ racial-lip-1108/src/App.js
@@ -1,32 +1,15 @@
 
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Aadicomp/Navbar/Navbar';
-import Footer from './Footer/Footer';
-import Topimg from './Aadicomp/Topimage/Topimg';
-import TrendingFundraisers from './Aadicomp/Trending/TrendingFundraisers';
-import Trendingcard from './Aadicomp/Trending/Trendingcard';
-import Start from './Aadicomp/start/Start';
-
-import Buttoncomp from './Aadicomp/Button/Button';
-import Monthly from './Aadicomp/Monthlydonation/Monthly';
-import Success from './Aadicomp/Sucess/Success';
+import Allcomp from './Aadicomp/Allcomp/Allcomp';
+import Footer from "./Footer/Footer"
+import FundForm from './Aadicomp/FundRaiser/FundForm';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-     <Topimg/>
-     <br/>
-     <TrendingFundraisers/>
-     <Start/>
-     <br/>
-     <br/>
-     <Buttoncomp/>
-     <Monthly/>
-     <Success/>
-      <Footer/>
-      
+    {/* <Allcomp/> */}
+    <FundForm/>
     </div>
   );
 }
