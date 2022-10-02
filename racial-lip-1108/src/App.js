f9785c32245fc83d8c8675a7e0ad89657a6b5477
Ajay Yadav <ajayyadav@Ajays-MacBook-Air.local>
2022-10-02 10:19:25 +0530

    update branch

--- racial-lip-1108/src/App.js
+++ racial-lip-1108/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HowItWork from './How It Works Page/HowItWork';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <HowItWork />
+      <Footer />
     </div>
   );
 }
